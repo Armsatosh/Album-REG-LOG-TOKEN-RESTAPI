d04af821b4aeaae1d0702fb0c2d4fe98d0801954
Gevorg Stepanyan <gev.steparm@gmail.com>
2019-07-26 18:05:17 +0400

    Configure Access-Control-Allow-Origin

--- public/index.php
+++ public/index.php
@@ -3,7 +3,23 @@
 use App\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    die();
+}
 
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+$ret = [
+    'result' => 'OK',
+];
+print json_encode($ret);
 require dirname(__DIR__).'/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
